9f79d155afcfe1bd8e5fed024d38debe5303d5cd
tianshiaimili <913540483@qq.com>
2015-08-13 01:13:26 +0800

    js和webview

--- src/com/example/annotationdemo/HTMLActivity.java
+++ src/com/example/annotationdemo/HTMLActivity.java
@@ -5,7 +5,13 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebChromeClient.CustomViewCallback;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.annotationdemo.bean.MyObject;
 
@@ -21,6 +27,8 @@ public class HTMLActivity extends Activity {
         webView = (WebView)this.findViewById(R.id.webView);
         //设置字符集编码
         webView.getSettings().setDefaultTextEncodingName("UTF-8");
+        webView.setWebChromeClient(new MyWebViewChromeClick());
+        webView.setWebViewClient(new MyWebViewClick());
         //开启JavaScript支持
         webView.getSettings().setJavaScriptEnabled(true);
         //传递一个Java对象，同时给他命名，这个对象可以在js中调用这个对象的方法
@@ -29,4 +37,60 @@ public class HTMLActivity extends Activity {
         String url = "file:///android_asset/index.html";
         webView.loadUrl(url);
     }
+    
+    
+    class MyWebViewClick extends WebViewClient{
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			return super.shouldOverrideUrlLoading(view, url);
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			
+			super.onPageFinished(view, url);
+		}
+		
+		
+    	
+    }
+    
+    class MyWebViewChromeClick extends WebChromeClient{
+
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			// TODO Auto-generated method stub
+			super.onProgressChanged(view, newProgress);
+		}
+
+		@Override
+		public void onShowCustomView(View view, CustomViewCallback callback) {
+			// TODO Auto-generated method stub
+			super.onShowCustomView(view, callback);
+		}
+
+		@Override
+		public void onHideCustomView() {
+			// TODO Auto-generated method stub
+			super.onHideCustomView();
+		}
+
+		@Override
+		public boolean onJsAlert(WebView view, String url, String message,
+				JsResult result) {
+			// TODO Auto-generated method stub
+			result.confirm();
+			return super.onJsAlert(view, url, message, result);
+		}
+
+		@Override
+		public boolean onJsConfirm(WebView view, String url, String message,
+				JsResult result) {
+			// TODO Auto-generated method stub
+			return super.onJsConfirm(view, url, message, result);
+		}
+    	
+    }
+    
 }
